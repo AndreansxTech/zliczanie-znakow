e2e38c78baaecd5742abdb5fd7dc613307ec2275
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-10-19 20:24:34 +0200

    Update zliczanie_znakow.cpp
    zrobiłem tak aby kiedy wpiszemy każdy znak oprócz ESC to żeby pokazał się on we wierszu poleceń a kiedy damy ESC to wypisuje że wpisano ESC i kończy działanie pętli

--- zliczanie_znakow.cpp
+++ zliczanie_znakow.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
-char znak=0;
+
 int main()
 {
+    char znak=0;
     while(znak!=27)
-    {
-        cout<<"Podaj liczbe: ";
-        znak=getch();
-        cout<<znak<<endl;
-    }
-    
+        {
+            cout<<"Podaj liczbe: ";
+            znak=getch();
+            if(znak==27)
+                cout<<"Wcisnieto ESC\n";
+            else
+                cout<<znak<<"\n";
+        }
     return 0;
 }
