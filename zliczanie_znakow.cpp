d8b80b459d333aa077429df58909a5f803fbee5f
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-10-19 22:40:07 +0200

    Update zliczanie_znakow.cpp

--- zliczanie_znakow.cpp
+++ zliczanie_znakow.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <stdio.h>
 #include <conio.h>
 using namespace std;
+const int ROZMIAR=999;
 char znak;
+int suma=0;
+int wprowadzone;
+int tablica[ROZMIAR];
+void oblicz_maks()
+{
+    for (int i=0; i<wprowadzone; i++)
+    {
+        suma=tablica[i]+suma;
+    }
+}
 int main()
 {
     while(znak!=27)
+    {
+        if (znak==27)
         {
-            cout<<"Podaj znak: ";
-            znak=getch();
-            if (znak!=27)
-                cout<<znak<<endl;
-            else
-                cout<<"podano esc\n";
+            cout<<"wprowadzono esc\n";
+
         }
+        else
+        {
+            cout<<"podaj liczbe: ";
+            tablica[wprowadzone]=getch(); znak=tablica[wprowadzone]; cout<<znak<<"\n"; wprowadzone++;
+        }
+    }
+    cout<<"wprowadzono "<<wprowadzone-1<<" liczb\n";
+    oblicz_maks();
+    cout<<"suma wprowadzonych liczb to: "<<suma<<endl;
     return 0;
 }
