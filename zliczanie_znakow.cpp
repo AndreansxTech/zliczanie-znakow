812cdf53e23bb4f8543b3573d29b071292f7a625
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-10-21 21:35:55 +0200

    Update zliczanie_znakow.cpp

--- zliczanie_znakow.cpp
+++ zliczanie_znakow.cpp
@@ -15,9 +15,7 @@ int main()
     int liczby[MAKS];
 
     for (int i=0; i<MAKS;i++)
-    {
         liczby[i]=0;
-    }
     char znak;
     cout<<"Zliczanie znakow. Wpisuj znaki a aby zakonczyc wcisnij ESC\n";
     while (true)
@@ -25,35 +23,27 @@ int main()
         cout<<"Podaj znak: ";znak=getch();
         if (znak==27)
             break;
-        else if (znak>='0'&&znak<='9')
-        {
+        else if (znak>='0'&&znak<='9'){
             cout<<znak;
             liczba=znak-'0'; cout<<endl; suma=suma+liczba;
             liczby[licznik]=liczba; licznik++;
-            cout<<liczba<<endl;
-        }
+            cout<<liczba<<endl;}
         else
-        {
             cout<<"Nie poprawny znak !\n";
-        }
     }
 
     kolorki(12);cout<<"Wpisano ESC\n";kolorki(7);cout<<" Wprowadzono ";kolorki(9);cout<<licznik;kolorki(7);cout<<" liczb\n";
     cout<<"Zapisane liczby:\n";
-    for (int i=0; i<licznik;i++)
-        {
-            kolorki(11);cout<<i+1<<".   ";kolorki(9); cout<<liczby[i]<<endl;
-        }
+    for (int i=0; i<licznik;i++) {
+            kolorki(11);cout<<i+1<<".   ";kolorki(9); cout<<liczby[i]<<endl; }
     kolorki(7);
     cout<<"Suma podanych liczb wynosi "<<suma<<endl;
     int maks,mini=liczby[0];
     for (int i=0; i<licznik; i++)
     {
-        if (liczby[i]<liczby[i+1])
-        {
+        if (liczby[i]<liczby[i+1]) {
             maks=liczby[i+1];
-            mini=liczby[i];
-        }
+            mini=liczby[i]; }
     }
     cout<<"Najwieksza z podanych liczb to ";kolorki(9);cout<<maks<<endl;kolorki(7);
     cout<<"Najmniejsza z podanych liczb to ";kolorki(9);cout<<mini<<endl;kolorki(7);
