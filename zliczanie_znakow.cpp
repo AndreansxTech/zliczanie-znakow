049b9474b6422518ad944539a895a61ee23d5b37
Acensxandrea <147649026+AndreansxTech@users.noreply.github.com>
2024-10-21 17:15:02 +0200

    Update zliczanie_znakow.cpp

--- zliczanie_znakow.cpp
+++ zliczanie_znakow.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <conio.h>
+#include <windows.h>
+HANDLE color=GetStdHandle(STD_OUTPUT_HANDLE);
 using namespace std;
+void kolorki(int kolor)
+{
+    SetConsoleTextAttribute(color,kolor);
+}
+
 int main()
 {
-    int liczba,suma,licznik;int MAKS=999;
+    int liczba,licznik=0;int MAKS=999;
+    float suma=0;
     int liczby[MAKS];
+
+    for (int i=0; i<MAKS;i++)
+    {
+        liczby[i]=0;
+    }
     char znak;
     cout<<"Zliczanie znakow. Wpisuj znaki a aby zakonczyc wcisnij ESC\n";
     while (true)
@@ -15,7 +28,8 @@ int main()
         else if (znak>='0'&&znak<='9')
         {
             cout<<znak;
-            liczba=znak-'0'; cout<<endl; suma=suma+liczba; licznik++;
+            liczba=znak-'0'; cout<<endl; suma=suma+liczba;
+            liczby[licznik]=liczba; licznik++;
             cout<<liczba<<endl;
         }
         else
@@ -23,7 +37,30 @@ int main()
             cout<<"Nie poprawny znak !\n";
         }
     }
-    cout<<"Wpisano ESC\n Wprowadzono "<<licznik<<" liczb\n";
+
+    kolorki(12);cout<<"Wpisano ESC\n";kolorki(7);cout<<" Wprowadzono ";kolorki(9);cout<<licznik;kolorki(7);cout<<" liczb\n";
+    cout<<"Zapisane liczby:\n";
+    for (int i=0; i<licznik;i++)
+        {
+            kolorki(11);cout<<i+1<<".   ";kolorki(9); cout<<liczby[i]<<endl;
+        }
+    kolorki(7);
     cout<<"Suma podanych liczb wynosi "<<suma<<endl;
-    return(0);
+    int maks,mini=liczby[0];
+    for (int i=0; i<licznik; i++)
+    {
+        if (liczby[i]<liczby[i+1])
+        {
+            maks=liczby[i+1];
+            mini=liczby[i];
+        }
+    }
+    cout<<"Najwieksza z podanych liczb to ";kolorki(9);cout<<maks<<endl;kolorki(7);
+    cout<<"Najmniejsza z podanych liczb to ";kolorki(9);cout<<mini<<endl;kolorki(7);
+    float srednia=0;
+    srednia=suma/licznik;
+    cout<<"Srednia podanych liczb to ";kolorki(9);cout<<srednia<<endl;kolorki(7);
+
+
+return(0);
 }
